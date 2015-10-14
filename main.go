0721ef49f233e17384f0d6a89677304aa9aa84bd
Raymond Wen <rx.wen218@gmail.com>
2015-10-14 21:54:08 +0800

    Init commit, implment firmware version list/download functions
    - Use mux package & net/http to implment it

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+const VAR_NAME_VERSION = "version"
+
+type VersionInfo struct {
+	Version  string `json:"version"`
+	Path     string `json:"path"`
+	Checksum string `json:"checksum"`
+}
+
+var versions = map[string]VersionInfo{
+	"v1.0.0": VersionInfo{
+		Version: "v1.0.0",
+		Path:    "./main.go",
+	},
+	"v1.0.1": VersionInfo{
+		Version: "v1.0.1",
+		Path:    "./main2.go",
+	},
+}
+
+func getAvailableVersions() map[string]VersionInfo {
+	return versions
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/version/{"+VAR_NAME_VERSION+"}", showVersion)
+	router.HandleFunc("/download/{"+VAR_NAME_VERSION+"}", download)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+	json.NewEncoder(w).Encode(getAvailableVersions())
+}
+
+func showVersion(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	version := vars[VAR_NAME_VERSION]
+	vi := getAvailableVersions()[version]
+	json.NewEncoder(w).Encode(vi)
+}
+
+func download(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	version := vars[VAR_NAME_VERSION]
+	vi := getAvailableVersions()[version]
+	if _, err := os.Stat(vi.Path); os.IsNotExist(err) {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, vi.Path+" not found")
+		return
+	}
+	reader, _ := os.Open(vi.Path)
+	fi, _ := reader.Stat()
+	w.Header().Set("Content-Disposition", "attachment; filename=a.txt")
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", strconv.FormatInt(fi.Size(), 10))
+	io.Copy(w, reader)
+}
